f3d7ba83a1cb55d89afd3372019f56a294a8dbf2
Ankit Mnakar <93331270+ankitmankar0@users.noreply.github.com>
2022-04-12 14:44:39 +0530

    Update Program.cs

--- Prime/Prime/Program.cs
+++ Prime/Prime/Program.cs
@@ -6,7 +6,7 @@ namespace PrimeNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number");
+            Console.WriteLine("Enter a number");  /// 3
             int number = Convert.ToInt32(Console.ReadLine());
             int result = Check_Prime(number);
             if (result == 0)
@@ -23,14 +23,14 @@ namespace PrimeNumber
         private static int Check_Prime(int number)
         {
             int i;
-            for (i = 2; i <= number - 1; i++)
+            for (i = 2; i <= number - 1; i++) // i = 2
             {
-                if (number % i == 0)
+                if (number % i == 0)  // if r = 0 then its not prime
                 {
                     return 0;
                 }
             }
-            if (i == number)
+            if (i == number) /// i3 = 3 then prime
             {
                 return 1;
             }
