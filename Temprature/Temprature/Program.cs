70671cfbd550408049d4d054a721f19b8980e826
Ankit Mnakar <93331270+ankitmankar0@users.noreply.github.com>
2022-04-12 13:14:41 +0530

    Update Program.cs

--- Temprature/Temprature/Program.cs
+++ Temprature/Temprature/Program.cs
@@ -5,18 +5,16 @@ namespace Temprature
 {
     class TemperatureConversion
     {
-        double celsius;
-        double fahrenheit;
         static void Main(string[] args)
         {
             double celsius;
             double fahrenheit;
-            Console.WriteLine("Enter value for celsius:");
+            Console.WriteLine("Enter value for celsius:"); //40
             celsius = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter value for Fahrenheit:");
+            Console.WriteLine("Enter value for Fahrenheit:");  // 80
             fahrenheit = Convert.ToDouble(Console.ReadLine());
-            double celToFah = (celsius * 9 / 5) + 32;
-            double fahToCel = (fahrenheit - 32) * 5 / 9;
+            double celToFah = (celsius * 9 / 5) + 32;  // 40*9/5=72+32=104
+            double fahToCel = (fahrenheit - 32) * 5 / 9; //80-32=48*5/9= 26.6
             Console.WriteLine("After celsius to fahrenheit conversion: {0}", celToFah);
             Console.WriteLine("After fahrenheit to celsius conversion: {0}", fahToCel);
 
