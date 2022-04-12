3846995e24a0d9c1f9788ed0dea8a7665d9e5b19
Ankit Mnakar <93331270+ankitmankar0@users.noreply.github.com>
2022-04-12 13:08:06 +0530

    Update Program.cs

--- Vending/Vending/Program.cs
+++ Vending/Vending/Program.cs
@@ -8,14 +8,14 @@ namespace VendingMachine
         {
             Console.WriteLine("minimum number of notes to give change");
             int Number = Convert.ToInt32(Console.ReadLine());
-            int remainder = Number;
+            int remainder = Number;  ///215
             int[] Array = new int[8] { 1000, 500, 100, 50, 10, 5, 2, 1 };
             foreach (int note in Array)
             {
                 int i = 0;
-                while (remainder >= note)
+                while (remainder >= note)  // 215 > 1000, 500 , 100 etc // r = 115 >100 // 15 > 100 go in result and check for other note//
                 {
-                    remainder -= note;
+                    remainder -= note; //  215-100 = 115 // 115-100=15 // 15-10=5 //
                     i++;
                 }
                 string result = i > 0 ? $"{note} X {i} = {note * i}" : "-";
